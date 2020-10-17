ebdeaa49be7937fbc3374c7e5cf9d5661982d129
kain555 <56169403+kain555@users.noreply.github.com>
2020-10-17 15:35:04 +0100

    Added DB model
    Addede DB Model with test table.
    
    Install:
    - EF Design
    - EF SQL Server
    - EF Tools
    
    Command to update DbContext Model:
     dotnet ef dbcontext scaffold "Server=elearnapp.database.windows.net;Database=eLearnDB;user id=elearnAdmin;password=ADlearn123!;" Microsoft.EntityFrameworkCore.SqlServer -o Model -f

---
+++ WebAPI/API/Model/TestTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace API.Model
+{
+    public partial class TestTable
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ WebAPI/API/Model/eLearnDBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace API.Model
+{
+    public partial class eLearnDBContext : DbContext
+    {
+        public eLearnDBContext()
+        {
+        }
+
+        public eLearnDBContext(DbContextOptions<eLearnDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TestTable> TestTables { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=elearnapp.database.windows.net;Database=eLearnDB;user id=elearnAdmin;password=ADlearn123!;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestTable>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("TestTable");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(10)
+                    .HasColumnName("name")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Surname)
+                    .HasMaxLength(10)
+                    .HasColumnName("surname")
+                    .IsFixedLength(true);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
