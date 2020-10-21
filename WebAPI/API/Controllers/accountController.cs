b8a46c95f9f14db11625bf3c0a722fb5eb2b6538
kain555 <56169403+kain555@users.noreply.github.com>
2020-10-21 21:35:06 +0200

    Added account controller
    Works, but not with Postman.

---
+++ WebAPI/API/Controllers/accountController.cs
@@ -0,0 +1,45 @@
+﻿using API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class accountController : baseController
+    {
+        public readonly eLearnDBContext _context;
+        public accountController(eLearnDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("newDisciple")]
+        public async Task<ActionResult<NewDisciple>> NewDisciple(int teacherId, DateTime createDate, int schoolId, int ClassId, string login, string name, string surname, string dateOfBirth, int pesel, string email, string address, string postalCode, string city)
+        {
+            var newD = new NewDisciple
+            {
+                Status = "Waiting for director",
+                TeacherId = teacherId,
+                CreateDate = createDate,
+                SchoolId = schoolId,
+                Login = login,
+                Name = name,
+                Surname = surname,
+                DateOfBirth = dateOfBirth,
+                Pesel = pesel,
+                Email = email,
+                Address = address,
+                PostalCode = postalCode,
+                City = city,
+                ClassId = ClassId
+            };
+            _context.NewDisciples.Add(newD);
+            await _context.SaveChangesAsync();
+
+            return newD;
+        }
+    }
+}
