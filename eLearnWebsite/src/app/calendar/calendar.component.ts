870126201ce7cf5cbb12d867f8d99b6549ae1a68
kain555 <56169403+kain555@users.noreply.github.com>
2020-12-13 18:56:04 +0100

    Last Stable 13.12.2020

--- eLearnWebsite/src/app/calendar/calendar.component.ts
+++ eLearnWebsite/src/app/calendar/calendar.component.ts
@@ -99,7 +99,6 @@ export class CalendarComponent implements OnInit {
         });
       });
       }
-      console.log(this.recordsCalendar);
       this.calendarEvents = this.recordsCalendar;
     });
   }
